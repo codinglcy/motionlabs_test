5359c971da854a91265a4d5df966ae5b14d6fc28
codinglcy <codinglcy@gmail.com>
2025-03-28 00:52:38 +0900

    ✨ hot reload setting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import "dotenv/config";
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app
     .listen(process.env.PORT)
     .then(() => console.log(`port ${process.env.PORT}에 연결되었습니다.`));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
